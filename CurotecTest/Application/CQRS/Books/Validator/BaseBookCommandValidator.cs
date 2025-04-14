2f8630354d9712db65d0350c5c4b4f86a0d52003
Marcelo Silva <mdrj10@hotmail.com>
2025-04-13 21:54:42 -0300

    Finishing touches and initial seed for user

--- CurotecTest/Application/CQRS/Books/Validator/BaseBookCommandValidator.cs
+++ CurotecTest/Application/CQRS/Books/Validator/BaseBookCommandValidator.cs
@@ -10,7 +10,7 @@ namespace Application.CQRS.Books.Validator
             RuleFor(x => x.Title).MaximumLength(50).WithMessage("Max value for Title is 50 characters")
                 .NotEmpty().WithMessage("Title is required");
             RuleFor(x => x.YearPublished).NotEmpty().WithMessage("Year Published is required");
-            RuleFor(x => x.AuthorId).NotEmpty().WithMessage("Year Published is required");
+            RuleFor(x => x.AuthorId).NotEmpty().WithMessage("Author is required");
         }
     }
 }
--- CurotecTest/Repository/Migrations/20250412205036_Changes.cs
+++ CurotecTest/Repository/Migrations/20250412205036_Changes.cs
@@ -13,6 +13,44 @@ namespace Repository.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            migrationBuilder.InsertData(
+        table: "User",
+        columns:
+        [
+            "Name",
+            "UserName",
+            "NormalizedUserName",
+            "Email",
+            "NormalizedEmail",
+            "EmailConfirmed",
+            "PasswordHash",
+            "SecurityStamp",
+            "ConcurrencyStamp",
+            "PhoneNumber",
+            "PhoneNumberConfirmed",
+            "TwoFactorEnabled",
+            "LockoutEnd",
+            "LockoutEnabled",
+            "AccessFailedCount"
+        ],
+        values:
+        [
+            "Admin",
+            "admin",
+            "ADMIN",
+            "curotec@admin.com",
+            "CUROTEC@ADMIN.COM",
+            false,
+            "AQAAAAIAAYagAAAAEIKWvFSGXxo8o5Uoyvc0TZpfyYf1UimH9nkgQ0lHRd4Z+55yh1Gr5XXG1zLyrZQBqA==",
+            "45J6LMIO2TKYLS66KKF6UKUPYHS5F2GW",
+            "732cc7ed-1f21-437b-98da-ba46f202a78a",
+            null,
+            false,
+            false,
+            null,
+            true,
+            0
+        ]);
         }
 
         /// <inheritdoc />
